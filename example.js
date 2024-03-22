2ed69b8b730a18f6f1d5125ead67af0de4d3cc3a
truong220 <truong210244@hnet.edu.vn>
2024-03-22 16:11:09 +0700

    haha

---
+++ example.js
@@ -0,0 +1,44 @@
+// const listNumber1=[];
+// console.log(listNumber1)
+// const listNumber2=new Array()
+// console.log(listNumber2)
+// const listNumber3=[1,'2',true,null];
+// // console.log(listNumber3);
+// const chars=['a','b','c','d'];
+// for(let i=0;i<chars.length;i++){
+//     console.log('value:' ,chars[i]);
+//     console.log('index: ',i);
+//     console.log('----------');
+    
+// }
+// for(let element of chars){
+//     console.log('element :',element);}
+//( push) them vao cuoi mang
+// console.log(chars);
+// chars.push('stringq');
+// (unshift) them vao dau mang 
+// console.log(chars);
+// chars.unshift('stringq');
+// (pop) xoa cuoi mang
+// chars.pop();
+// (shift) xoa dau mang
+// chars.shift();
+
+// (splice) xoa phan tu bat ki  xoa cuoi mang length -1
+// console.log(chars)
+// chars.splice(2,1)// xoa 1 phan tu o vi tri 2
+// console.log(chars)
+// (splice) them phan tu bat ki  them  cuoi mang length -1
+// console.log(chars)
+// chars.splice(2,1, 'string')// them phan tu string vao vi tri 2 thay the vi tri so 1
+// console.log(chars)
+
+
+
+
+// kiem tra co trong mang hay k 
+// const chars=['a','b','c','d'];
+
+
+
+
